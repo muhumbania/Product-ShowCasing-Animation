bf4a6881ef66bb1f85cf51656b5c0d937704000d
muhumbania <franklinmuhumbania@gmail.com>
2024-04-03 19:35:21 +0300

    ring animation done, opacity made dynamic

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import { gsap } from "gsap";
 
-let xPos = 0;
+let counter = 0;
+let imgId = 1;
 
 gsap.timeline()
-    .set('.ring', { rotationY:180, cursor:'grab' }) //set initial rotationY so the parallax jump happens off screen
+    .set('.ring', { rotationY:0, cursor:'grab' }) //set initial rotationY so the parallax jump happens off screen
     .set('.img',  { // apply transform rotations to each image
       rotateY: (i)=> i*-36,
       transformOrigin: '50% 50% 500px',
       z: -500,
-      backgroundImage:(i)=>'url(https://picsum.photos/id/'+(i+32)+'/600/400/)',
-      backgroundPosition:(i)=>getBgPos(i),
+    //   backgroundPosition:(i)=>getBgPos(i),
       backfaceVisibility:'hidden'
     })    
     .from('.img', {
@@ -19,46 +18,23 @@ gsap.timeline()
       stagger:0.1,
       ease:'expo'
     })
-    .add(()=>{
-      $('.img').on('mouseenter', (e)=>{
-        let current = e.currentTarget;
-        gsap.to('.img', {opacity:(i,t)=>(t==current)? 1:0.5, ease:'power3'})
-      })
-      $('.img').on('mouseleave', (e)=>{
-        gsap.to('.img', {opacity:1, ease:'power2.inOut'})
-      })
-    }, '-=0.5')
-
-$(window).on('mousedown touchstart', dragStart);
-$(window).on('mouseup touchend', dragEnd);
-      
-
-function dragStart(e){ 
-  if (e.touches) e.clientX = e.touches[0].clientX;
-  xPos = Math.round(e.clientX);
-  gsap.set('.ring', {cursor:'grabbing'})
-  $(window).on('mousemove touchmove', drag);
-}
-
-
-function drag(e){
-  if (e.touches) e.clientX = e.touches[0].clientX;    
-
-  gsap.to('.ring', {
-    rotationY: '-=' +( (Math.round(e.clientX)-xPos)%360 ),
-    onUpdate:()=>{ gsap.set('.img', { backgroundPosition:(i)=>getBgPos(i) }) }
-  });
+    // .add(()=>{
+    //   $('.btn').on('click', function(){
+       
+    //   });
+    // }, '-=0.5')
   
-  xPos = Math.round(e.clientX);
-}
 
-
-function dragEnd(e){
-  $(window).off('mousemove touchmove', drag);
-  gsap.set('.ring', {cursor:'grab'});
-}
-
-
-function getBgPos(i){ //returns the background-position string to create parallax movement in each image
-  return ( 100-gsap.utils.wrap(0,360,gsap.getProperty('.ring', 'rotationY')-180-i*36)/360*500 )+'px 0px';
-}
+    $('.btn').on('click', function(){
+        counter += 36;
+        imgId += 1;
+        if(imgId > 10){
+            imgId = 1;
+        } 
+        gsap.to('.ring', { rotationY: counter});
+        
+        let current = document.getElementById(`${imgId}`);
+        // alert(current);
+        
+        gsap.to('.img', {opacity:(i,t)=>(t==current)? 1:0.5, ease:'power3'})
+    });
